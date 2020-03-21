e81d8413df34a8e1658ab382cbb3edbe3ad31714
Juani Galan <juan@lumapps.com>
2020-03-21 12:00:09 +0100

    added tweets

--- src/App/Donations/Donation/index.jsx
+++ src/App/Donations/Donation/index.jsx
@@ -8,17 +8,27 @@ import { translate } from '../../../translations';
 const CLASSNAME = 'donation';
 const Donation = (props) => {
     const { lang } = React.useContext(AppContext);
-    const { name, image, description, link } = props;
+    const { name, image, description, link, tweet } = props;
 
     return (
         <Card className={CLASSNAME}>
             <img className={`${CLASSNAME}__image`} src={image} alt={name} />
             <div className={`${CLASSNAME}__content`}>
-                <Title className={`${CLASSNAME}__title`} importance={2} theme="dark">{name}</Title>
-                <p className={`${CLASSNAME}__description`}>{description[lang]}</p>
-                <Button className={`${CLASSNAME}__action`} asLink href={link} target="_blank">
-                    {translate('DONATE', lang)}
-                </Button>
+                { name && <Title className={`${CLASSNAME}__title`} importance={2} theme="dark">{name}</Title> }
+                { description && <p className={`${CLASSNAME}__description`} dangerouslySetInnerHTML={{ __html: description[lang] }} /> }
+                {
+                    link && (
+                        <Button className={`${CLASSNAME}__action`} asLink href={link} target="_blank">
+                            {translate('DONATE', lang)}
+                        </Button>
+                    )
+                }
+                {
+                    tweet && (
+                        <div className={`${CLASSNAME}__tweet`} dangerouslySetInnerHTML={{ __html: tweet }}>
+                        </div>
+                    )
+                }
             </div>
         </Card>
     );
