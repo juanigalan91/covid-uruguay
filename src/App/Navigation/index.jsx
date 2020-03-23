ba9db75b807a2d92386189fe1c23c1528c04158b
Juani Galan <juan@lumapps.com>
2020-03-23 18:36:46 +0100

    css fixes for navigation

--- src/App/Navigation/index.jsx
+++ src/App/Navigation/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { AppContext } from '../../components/Context';
 import { Link } from 'react-router-dom';
 import routes from '../routes.json';
+import { translate } from '../../translations';
   
-
 const CLASSNAME = 'navigation';
 const Navigation = () => {
     const { lang } = React.useContext(AppContext);
 
     return (
         <nav className={CLASSNAME} role="navigation">
-            <Link to={routes.colaborate}>Colaborar</Link>
+            <Link className={`${CLASSNAME}__link`} to={routes.home}>{translate('DONATIONS', lang)}</Link>
+            <Link className={`${CLASSNAME}__link`} to={routes.colaborate}>{translate('COLABORATE', lang)}</Link>
         </nav>
 
     );
